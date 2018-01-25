08c1002ae427657b47b8c43cc38fb0df6b9655a9
Arie Tando <13510018@std.stei.itb.ac.id>
2018-01-25 07:28:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ def move(board, index, value = "X")
 end
 
 def turn(board)
-  loop
+  loop do
     puts "Please enter 1-9:"
     input = input_to_index(gets.strip)
     if (valid_move?(board,input))
